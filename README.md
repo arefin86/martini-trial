39b709619682eb1e15f77cf157cd621b41564b55
arefin86 <arefin.86@gmail.com>
2021-02-08 20:11:33 -0800

    getting meshes

---
+++ README.md
@@ -0,0 +1,5 @@
+`npm install`
+`webpack`
+`webpack serve --open`
+
+Go to `localhost:9000`
--- src/index.js
+++ src/index.js
@@ -2,13 +2,43 @@ import * as  THREE from 'three';
 import * as fs from 'fs';
 import Martini from '@mapbox/martini'; 
 import {mapboxTerrainToGrid} from './utils.js';
+import {arrayBufferToBase64} from './arrayBufferToBase64.js';
 import {PNG} from 'pngjs/browser';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const fuji = PNG.sync.read(fs.readFileSync('img/fuji.png'));
-const terrain = mapboxTerrainToGrid(fuji);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const martini = new Martini(fuji.width + 1);
-const tile = martini.createTile(terrain);
-const mesh = tile.getMesh(500);
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-console.log(mesh);
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+
+fetch('img/fuji.png')
+    .then(res=> {
+        res.arrayBuffer().then((buffer)=>{
+            const imageData = buffer;
+
+            let image = new PNG({ filterType: 4}).parse(imageData, (error, data)=>{
+                const terrain = mapboxTerrainToGrid(image);
+                const martini = new Martini(image.width + 1);
+                const tile = martini.createTile(terrain);
+                const mesh = tile.getMesh(500);
+                console.log(mesh);
+            })
+        })
+    });
+
+
+    animate();
